540ce4f3209a7fec35e0a2396fe514312ccaf8c4
Benni-beep <krikelkr4kel@gmail.com>
2026-05-15 11:17:27 +0200

    fix: extract calculate_conversion from converter_home

--- _2/currency_converter/converter/views.py
+++ _2/currency_converter/converter/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Currency
 
+def calculate_conversion(amount, from_rate, to_rate):
+    if from_rate == 0: return 0
+    return (amount / from_rate) * to_rate
+
 def converter_home(request):
     currencies = Currency.objects.all()
     result = None
@@ -14,14 +18,14 @@ def converter_home(request):
         to_id_str = request.POST.get('to_currency_id')
 
         if amount_str and from_id_str and to_id_str:
-            amount = amount_str # Keep as string for the input field value
+            amount = amount_str
             from_id = int(from_id_str)
             to_id = int(to_id_str)
             
             from_curr = Currency.objects.get(id=from_id)
             to_curr = Currency.objects.get(id=to_id)
             
-            result = (float(amount) / from_curr.exchange_rate) * to_curr.exchange_rate
+            result = calculate_conversion(amount, from_curr.exchange_rate, to_curr.exchange_rate)
             
     context = {
         'currencies': currencies, 
